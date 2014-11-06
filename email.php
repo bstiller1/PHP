af8fba5e14b75fe1533b01418afac1665125449f
bstiller1 <stiller.blake@gmail.com>
2014-11-06 11:26:55 -0500

    PHP
    Added isset() to email form.

--- email.php
+++ email.php
@@ -1,25 +1,25 @@
 <?php
 // if post variables exist
-if($_POST['toaddress']){
+if(isset($_POST['toaddress'])){
 	// shortform the variables
 $to = $_POST['toaddress'];
 }else{
 echo "The \"To Address\" was not submitted, please go <a href='javascript:history.go(-1);' title='Back'>back</a> and try again";	
 }
-if($_POST['subject']){
+if(isset($_POST['subject'])){
 $subject = $_POST['subject'];
 }else{
 echo "The 'Subject' was not submitted, please go <a href='javascript:history.go(-1);' title='Back'>back</a> and try again";	
 }
-if($_POST['message']){
+if(isset($_POST['message'])){
 $message = $_POST['message'];
 }else{
 echo "The 'Message' was not submitted, please go <a href='javascript:history.go(-1);' title='Back'>back</a> and try again";	
 }
-if($_POST['from']){
+if(isset($_POST['from'])){
 $from = $_POST['from'];
 }
-if($_POST['cc']){
+if(isset($_POST['cc'])){
 $cc = $_POST['cc'];
 }
 // To send HTML mail, the Content-type header must be set
@@ -32,7 +32,6 @@ $headers .= "CC: " . $cc . "\r\n";
 if(!mail($to, $subject, $message, $headers)){
 	echo "Your email could not be sent";
 }else{
-
 // Let the user know email has been sent
 echo "Your mail has been sent.";
 }
