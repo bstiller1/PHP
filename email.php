7558661ecf78948cbf08e1add3b7e7a7a9c57876
bstiller1 <stiller.blake@gmail.com>
2014-11-06 11:21:17 -0500

    PHP
    Added PHP/MySQL and Email form and script.

---
+++ email.php
@@ -0,0 +1,40 @@
+<?php
+// if post variables exist
+if($_POST['toaddress']){
+	// shortform the variables
+$to = $_POST['toaddress'];
+}else{
+echo "The \"To Address\" was not submitted, please go <a href='javascript:history.go(-1);' title='Back'>back</a> and try again";	
+}
+if($_POST['subject']){
+$subject = $_POST['subject'];
+}else{
+echo "The 'Subject' was not submitted, please go <a href='javascript:history.go(-1);' title='Back'>back</a> and try again";	
+}
+if($_POST['message']){
+$message = $_POST['message'];
+}else{
+echo "The 'Message' was not submitted, please go <a href='javascript:history.go(-1);' title='Back'>back</a> and try again";	
+}
+if($_POST['from']){
+$from = $_POST['from'];
+}
+if($_POST['cc']){
+$cc = $_POST['cc'];
+}
+// To send HTML mail, the Content-type header must be set
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$headers .= "From: " . $from . "\r\n";
+$headers .= "CC: " . $cc . "\r\n";
+
+// send mail
+if(!mail($to, $subject, $message, $headers)){
+	echo "Your email could not be sent";
+}else{
+
+// Let the user know email has been sent
+echo "Your mail has been sent.";
+}
+
+?>
